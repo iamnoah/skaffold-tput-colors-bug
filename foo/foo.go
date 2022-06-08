5f2b7aa6142e08592c5be49cfc26c1bfc61a35ee
Noah Perks Sloan <noah@hackedu.io>
2022-06-08 12:13:38 -0500

    give vendor something to do

--- foo/foo.go
+++ foo/foo.go
@@ -1,5 +1,18 @@
 package main
 
+// give mod vendor something to do
+import (
+	_ "github.com/getsentry/sentry-go"
+	_ "github.com/gorilla/mux"
+	_ "github.com/opentracing-contrib/go-stdlib/nethttp"
+	_ "k8s.io/api/core/v1"
+	_ "k8s.io/apimachinery/pkg/runtime"
+	_ "k8s.io/apimachinery/pkg/types"
+	_ "k8s.io/apimachinery/pkg/util/intstr"
+	_ "k8s.io/utils/pointer"
+	_ "sigs.k8s.io/controller-runtime/pkg/client"
+)
+
 func main() {
 	println("hello world")
 }
